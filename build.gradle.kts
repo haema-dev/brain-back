b06aca7121939319b9a9b47d3ed42375d1905acf
SUA <haemahmtea@gmail.com>
2024-05-09 10:55:21 +0900

    add private submodule

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,26 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import java.io.File
+
+// Compiletime 이 아닌 Runtime 시에 동작하므로 buildscript 에 의존성 추가
+// Gradle 초기 설정 단계에서 클래스 경로가 설정되어야 하므로 buildscript 블록은 스크립트 파일의 상단에 위치
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.fasterxml.jackson.core:jackson-databind:2.15.0")
+        classpath("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.0")
+    }
+}
+
+// 메인 프로젝트와 경로를 맞춰줘야 런타임 시에 정상적으로 주입이 된다
+sourceSets {
+    val main by getting {
+        resources.srcDirs("src/main/resources", "security-module/brain-back/src/main/resources")
+    }
+}
 
 plugins {
     application
@@ -22,6 +44,16 @@ application {
     mainClass.set("power.brain.BrainApplicationKt")
 }
 
+// gradle 에서 yml 을 읽어오기 위한 설정
+tasks.register("readYaml") {
+    doLast {
+        val file = File("security-module/brain-back/src/main/resources/application.yml")
+        val mapper = ObjectMapper(YAMLFactory())
+        val config = mapper.readValue(file, Map::class.java)
+        println(config)
+    }
+}
+
 repositories {
     mavenCentral()
 }
@@ -39,6 +71,11 @@ dependencies {
 //    implementation("org.mapstruct:mapstruct:1.8.22")
     // Swagger
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")// openApi 와 SwaggerUI 를 통합하기 위해 추가
+
+
+    // Jackson yaml
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.0")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
